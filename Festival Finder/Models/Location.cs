b7e42cec57d6907c819e0d512b8b51c8637ac5a9
Nigel Hendricks <noah@Noahs-MacBook-Pro.local>
2023-04-26 10:59:31 -0500

    second push

--- Festival Finder/Models/Location.cs
+++ Festival Finder/Models/Location.cs
@@ -3,9 +3,33 @@ namespace Festival_Finder.Models
 {
 	public class Location
 	{
+		public int Id { get; set; }
+		public string Street { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+
+
 		public Location()
 		{
 		}
-	}
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Location location &&
+                   Street == location.Street &&
+                   City == location.City &&
+                   State == location.State;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Street, City, State);
+        }
+
+        public override string? ToString()
+        {
+            return ToString();
+        }
+    }
 }
 
